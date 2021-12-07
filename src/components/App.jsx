304c9930dd312a7a3ff1767d9d5db6f1f25cb4ee
starkpero <anishgupta0066@gmail.com>
2021-12-07 22:02:58 +0530

    boilerplate for different components and navigation

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
-import {BrowserRouter, Route} from 'react-router-dom';  
+import {BrowserRouter, Route,Routes, Link} from 'react-router-dom';  
+
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamShow from './streams/StreamShow';
+import StreamList from './streams/StreamList';
 
 
 const App = ()=>{
     return <div>
-        App
+        <BrowserRouter>
+        <div>
+            <Routes>
+            <Route path="/" exact element={<StreamList/>}/>
+            <Route path="/streams/new" exact element={<StreamCreate/>}/>
+            <Route path="/streams/edit" exact element={<StreamEdit/>}/>
+            <Route path="/streams/delete" exact element={<StreamDelete/>}/>
+            <Route path="/streams/show" exact element={<StreamShow/>}/>
+            </Routes>
+        </div>
+        </BrowserRouter>
     </div>
 };
 
