f3d89c5dc6a97374f3e56df013390d7177bf24ea
starkpero <anishgupta0066@gmail.com>
2021-12-10 22:30:33 +0530

    wired up json-server for REST based operations,also added redux thunk and axios for asyncronous network requests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+import streams from "../apis/streams";
+import { CREATE_STREAM } from "./types";
+
 export const signIn = (userId)=>{
     return {
         type : 'SIGN_IN',
@@ -10,4 +13,11 @@ export const signOut = ()=>{
     return {
         type : 'SIGN_OUT'
     };
+};
+
+
+export const createStream = (formValues)=>async dispatch=>{
+    const response = await streams.post('/streams',formValues);
+
+    dispatch({type : CREATE_STREAM , payload : response.data});
 };
--- src/components/streams/StreamCreate.jsx
+++ src/components/streams/StreamCreate.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Field,reduxForm} from 'redux-form';
+import {connect} from 'react-redux';
+import {createStream} from '../../actions';
 
 class StreamCreate extends React.Component{
     renderError({error, touched}){
@@ -24,8 +26,9 @@ class StreamCreate extends React.Component{
         );
     }
 
-    onSubmit(formValues){
-        console.log(formValues);
+    onSubmit = (formValues)=>{
+        //console.log(formValues);
+        this.props.createStream(formValues);
     }
 
     render(){
@@ -55,7 +58,10 @@ const validate = (formValues)=>{
 };
 
 
-export default reduxForm({
+const formWrapped =  reduxForm({
     form : 'streamCreate',
     validate
 })(StreamCreate);
+
+
+export default connect(null , {createStream})(formWrapped);
